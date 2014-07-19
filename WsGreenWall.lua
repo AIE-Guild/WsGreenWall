8655d965fa56ae946438619e9585103dbaab6da1
Mark Rogaski <mrogaski@pobox.com>
2014-07-19 12:30:16 -0400

    Added handlers for EditBox updates. (#4)

--- WsGreenWall.lua
+++ WsGreenWall.lua
@@ -54,12 +54,14 @@ local CHAN_OFFICER  = 2
 -- Default configuration values
 --
 local defaultOptions = {
-    bTag            = true,
-    bAchievement    = false,
-    bRoster         = true,
-    bRank           = false,
-    bOfficerChat    = false,
-    bDebug          = false,
+    bDebug              = false,
+    bTag                = true,
+    bAchievement        = false,
+    bRoster             = true,
+    bRank               = false,
+    bOfficerChat        = false,
+    sOfficerChatChannel = "",
+    sOfficerChatKey     = "",
 }
 
  
@@ -147,6 +149,7 @@ function WsGreenWall:new(o)
     for k, v in pairs(defaultOptions) do
         self.options[k] = v
     end
+    self.version        = nil
     self.ready          = false
     self.player         = nil
     self.guild          = nil
@@ -201,6 +204,7 @@ function WsGreenWall:OnLoad()
     -- load our form file
 	self.xmlDoc = XmlDoc.CreateFromFile("WsGreenWall.xml")
 	self.xmlDoc:RegisterCallback("OnDocLoaded", self)
+	self.version = XmlDoc.CreateFromFile("toc.xml"):ToTable().Version
 end
 
 
@@ -217,6 +221,7 @@ function WsGreenWall:OnDocLoaded()
 			return
 		end
 		
+		self.wndMain:FindChild("Title"):SetText(string.format("GreenWall v%s", self.version))
 	    self.wndMain:Show(false, true)
 
 		-- if the xmlDoc is no longer needed, you should set it to nil
@@ -514,6 +519,8 @@ function WsGreenWall:OpenConfigForm()
     self.wndMain:FindChild("ToggleOptionRoster"):SetCheck(self.scratch.bRoster)
     self.wndMain:FindChild("ToggleOptionRank"):SetCheck(self.scratch.bRank)
     self.wndMain:FindChild("ToggleOptionOfficerChat"):SetCheck(self.scratch.bOfficerChat)
+    self.wndMain:FindChild("InputOptionOfficerChatChannel"):SetText(self.scratch.sOfficerChatChannel)
+    self.wndMain:FindChild("InputOptionOfficerChatKey"):SetText(self.scratch.sOfficerChatKey)
     self.wndMain:FindChild("ToggleOptionDebug"):SetCheck(self.scratch.bDebug)
     
     -- Future features
@@ -530,6 +537,13 @@ function WsGreenWall:OnToggleOption(handler, control)
     local index = string.gsub(name, "ToggleOption(%w+)", "b%1")
     self.scratch[index] = not self.scratch[index]
     self.wndMain:FindChild(name):SetCheck(self.scratch[index])
+end
+
+-- Update handling
+function WsGreenWall:OnUpdateOption(handler, control, string)
+    local name = control:GetName()
+    local index = string.gsub(name, "InputOption(%w+)", "s%1")
+    self.scratch[index] = string
 end
 
 -- when the OK button is clicked
